7db5c821c0ac8bc8914a65473f8cc13e15b17f9b
anandclicks <146056018+anandclicks@users.noreply.github.com>
2024-08-08 12:40:32 +0530

    footer css changed

--- src/components/Foorertwo.jsx
+++ src/components/Foorertwo.jsx
@@ -41,7 +41,7 @@ function Foorertwo() {
             </ul>
           </div>
           <div className="col-md-3 mb-4">
-            <h6 className="custom-footer-heading">Connect Us</h6>
+            <h6 className="custom-footer-heading fs-3 fwnormal">Connect Us</h6>
             <div class="line-with-dots"><div class="line"></div></div>
            <ul className='flex flex-col gap-2'>
            <Link>Instagram</Link>
